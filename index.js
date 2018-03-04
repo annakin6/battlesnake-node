9244b063b0e80fc4c62b8c443b8dcc0502bd181a
annakin6 <root@saiga.localdomain>
2018-03-03 16:59:53 -0800

    revert

--- index.js
+++ index.js
@@ -129,31 +129,10 @@ app.post('/start', (request, response) => {
 })
 
 function setSurroundingValues(x, y, originalValue, iters) {
-  if(x + 1 < w + 2) {
-    ourGrid[x + 1][y + 0] += originalValue/2;
-  }
-  if(y - 1 > 0) {
-    ourGrid[x + 0][y - 1] += originalValue/2;
-  }
-  if(x - 1 > 0) {
-    ourGrid[x - 1][y + 0] += originalValue/2;
-  }
-  if(y + 1 < h + 2) {
-    ourGrid[x - 0][y + 1] += originalValue/2;
-  }
-
-  if(x + 2 < w + 2) {
-    ourGrid[x + 2][y + 0] += originalValue/4;
-  }
-  if(y - 2 > 0) {
-    ourGrid[x + 0][y - 2] += originalValue/4;
-  }
-  if(x - 2 > 0) {
-    ourGrid[x - 1][y + 0] += originalValue/4;
-  }
-  if(y + 2 < h + 2) {
-    ourGrid[x - 0][y + 2] += originalValue/4;
-  }
+  ourGrid[x + 1][y + 0] += originalValue/2;
+  ourGrid[x + 0][y - 1] += originalValue/2;
+  ourGrid[x - 1][y + 0] += originalValue/2;
+  ourGrid[x - 0][y + 1] += originalValue/2;
 }
 // function setSurroundingValues(x, y, originalValue, iters) {
 //   for (let i = 1; i < iters; i++) {
