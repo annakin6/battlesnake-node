7b0470415044b00219473da9465d9ca21307f0e2
annakin6 <root@saiga.localdomain>
2018-03-03 16:05:01 -0800

    Pick a random prequel taunt

--- index.js
+++ index.js
@@ -23,13 +23,64 @@ app.use(poweredByHandler)
 var h, w, gameId;
 
 // values for the grid calculations
-var placeholderValue = -1;
+var placeholderValue = 0;
 var wallValue = -100;
-var foodValue = 100;
+var foodValue = 90;
 var bodyValue = -100;
-var enemySmallerValue;
+var enemySmallerValue = 100;
 var enemyLargerValue = -100;
-var enemyBodyValue;
+var taunts = [
+  "Yousa thinking yousa people ganna die?",
+  "I don’t care what universe you’re from, that’s got to hurt!",
+   "Love won’t save you, Padme. Only my new powers can do that.",
+  "…Don’t try it, Anakin. I have the high ground!",
+  "There’s always a bigger fish.",
+  "I’m haunted by the kiss that you should never have given me.",
+  "Are you an angel?",
+  "I don’t like sand. It’s coarse and rough and irritating and it gets everywhere.",
+  "Ye gods, whatta meesa sayin’?",
+   "I sense Count Dooku.",
+  "Ani? My goodness, you’ve grown!",
+  "How wude!",
+  "I can’t take Dooku alone! I need you!",
+  "I’ve been wondering… what are midi-chlorians?",
+  "Chesco, Sebulba. Chipoka oomen geesa. Me teesa radical fbombati chop chawa.",
+  "I have the POWER! UNLIMITED… POWER!",
+  "Droidekas!",
+  "Uh! So uncivilized.",
+  "Now this is pod racing!",
+  "So this is how liberty dies… with thunderous applause.",
+  "…It is only natural. He cut off your arm, and you wanted revenge.",
+  "Always two there are, no more, no less.",
+  "Mom, you said that the biggest problem in the universe is no one helps each other.",
+  "He owes me what you’d call a ‘life-debt.’ Your gods demand that his life belongs to me.",
+  "From my point of view, the Jedi are evil!",
+  "I thought we had decided not to fall in love. That we’d be forced to live a lie and that it would destroy our lives.",
+  "A vergence, you say?",
+  "Now that I’m with you again, I’m in agony. My heart is beating, hoping that that kiss will not become a scar.",
+  "No loose wire jokes.",
+  "Your mother had gone out early, like she always did, to pick mushrooms that grow on the vaporators.",
+  "For reasons we can’t explain, we are losing her.",
+  "…Well, then you really are lost!",
+  "He said… you killed younglings!",
+  "What if the democracy we thought we were serving no longer exists, and the Republic has become the very evil we have been fighting to destroy?",
+  "I have waited a long time for this moment, my little green friend.",
+  "…I’ll try spinning. That’s a good trick. Whoa-ah!",
+  "Train yourself to let go… of everything you fear to lose.",
+  "There was no father. I carried him, I gave birth, I raised him. I can’t explain what happened.",
+  "You were banished because you were clumsy?",
+  "You are in my very soul, tormenting me…",
+  "…We used to come here for school retreat. We would swim to that island every day. I love the water. We used to lie out on the sand and let the sun dry us and try to guess the names of the birds singing.",
+  "At an end your rule is, and not short enough was it.",
+  "Ray shields!",
+  "Just being around her again is… intoxicating.",
+  "Your new Empire?",
+  "Symbionts?",
+  "They live inside me?",
+  "I don’t understand.",
+  "Your presence is soothing.",
+  "…We live in a real world, come back to it. You’re studying to become a Jedi, I’m… I’m a senator."
+]
 
 // variables for snake
 var currentDirection;
@@ -57,10 +108,17 @@ app.post('/start', (request, response) => {
   // Assigns value of wall to the wall values
   for (let i = 0; i < w + 2; i++) {
     ourGrid[i][0] = wallValue;
+    if(ourGrid[i+1][1] != null) {
+      ourGrid[i+1][1] = wallValue / 2;
+    }
     ourGrid[i][h+1] = wallValue;
+    if(ourGrid[i+1][h-1] != null) {
+      ourGrid[i+1][h-1] = wallValue / 2;
+    }
   }
   for (let i = 0; i < h + 2; i++) {
     ourGrid[0][i] = wallValue;
+    ourGrid[1][i+1] = 
     ourGrid[w+1][i] = wallValue;
   }
   
@@ -94,8 +152,12 @@ function updateGrid(snakes, food, ourSnake) {
     }
     console.log("after setting body values");
     if (snakes[i].id != ourSnake.id) {
-      // Assigns value of their snake head to the grid
-      ourGrid[snakes[i].body.data[0].x + 1][snakes[i].body.data[0].y + 1] = enemySmallerValue;
+      // Assigns value of their snake head to the grid, based on size of snake
+      if(snakes[i].length < ourSnake.length) {
+        ourGrid[snakes[i].body.data[0].x + 1][snakes[i].body.data[0].y + 1] = enemySmallerValue;
+      } else {
+        ourGrid[snakes[i].body.data[0].x + 1][snakes[i].body.data[0].y + 1] = enemyLargerValue;
+      }
     }
     console.log("after setting head value");
   }
@@ -170,7 +232,7 @@ app.post('/move', (request, response) => {
   // Response data
   const data = {
     move: nextMove, // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
+    taunt: taunts[Math.floor(Math.random() * taunts.length)], // optional, but encouraged!
   }
 
   return response.json(data)
